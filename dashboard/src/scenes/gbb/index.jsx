d1bf201ac04b8be993b63c83d231f6724482e0a8
wxy10 <xinyingwong21@gmail.com>
2024-04-13 17:15:38 +0800

    modified colour and %change box in gbb

--- dashboard/src/scenes/gbb/index.jsx
+++ dashboard/src/scenes/gbb/index.jsx
@@ -46,6 +46,7 @@ const GBB = () => {
             subtitle="Total Revenue of Bundle"
           />
         </Box>
+
         <Box
           gridColumn="span 3"
           backgroundColor={colors.primary[400]}
@@ -59,7 +60,7 @@ const GBB = () => {
           <Typography
             variant="h5" 
             sx={{ 
-              color: colors.greenAccent[500],
+              color: colors.greenAccent[300],
               textAlign: "left",
               marginTop: "10px",
               marginBottom: "20px"
@@ -79,42 +80,62 @@ const GBB = () => {
           </Box>
         </Box>
 
-
-
         <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          // alignItems="center"
+          justifyContent="center"
+        >
+          <StatBoxGbb
+            title="+20%"
+            subtitle="% Change of MFLG Revenue"
+          />
+        </Box>
+        
+        {/* with bar chart */}
+        {/* <Box
           gridColumn="span 3"
           backgroundColor={colors.primary[400]}
           display="grid"
           gridTemplateRows="auto 1fr" // Divide the grid into two rows, the first row adjusts to its content, the second row takes the remaining space
           height="100%" // Ensure the Box fills the entire height of the parent
           alignItems="center"
+          justifyContent="center"
+          textAlign="center" // Center-align text
           
-        >
+        > */}
           {/* Text at the top */}
-          <Typography
+          
+          {/* <Typography
             variant="h5" 
             sx={{ 
               color: colors.greenAccent[500],
+              textAlign: "center",
               marginTop: "10px",
-              marginBottom: "20px",
-              textAlign: "center"
+              marginBottom: "20px"
             }}
+            // mt="25px" // Add margin-top to create space between text and top of the Box
+            
           >
             % Change of MFLG Revenue
-          </Typography>
+          </Typography> */}
 
           {/* Grid container for the number and bar chart */}
-          <Box 
+          {/* <Box 
             display="grid" 
             gridTemplateColumns="20% 80%" 
-            margin="0"
-          >
+            margin="auto"
+            width="100%"
+            
+          > */}
             {/* Number */}
-            <Box
+            {/* <Box
               display="flex"
               justifyContent="center"
               alignItems="center"
               textAlign="center"
+              
             >
               <Typography
                 variant="h5" 
@@ -126,10 +147,10 @@ const GBB = () => {
               >
                 +20%
               </Typography>
-            </Box>
+            </Box> */}
             
             {/* Bar Chart */}
-            <Box
+            {/* <Box
               display="flex"
               justifyContent="center"
               alignItems="center"
@@ -139,13 +160,19 @@ const GBB = () => {
               <BarChartGbb isDashboard={true} />
             </Box>
           </Box>
-        </Box>
+        </Box> */}
 
       </Box>
 
+      
+
       {/* Header for Peak Period */}
       <Box mt={5} mb={2}>
-        <Typography variant="h3" component="div">
+        <Typography 
+          variant="h3" 
+          component="div" 
+          fontWeight="bold"
+        >
           Peak Period: [Add your peak period text here]
         </Typography>
       </Box>
@@ -156,14 +183,32 @@ const GBB = () => {
           {/* Line Chart */}
           <Grid item xs={12} md={6}>
             {/* grid item to occupy full width of container on XS screen and half the width on medium screen */}
-            <Header subtitle="Best Bundle Pricing" />
+            <Typography
+              variant="h5" 
+              fontWeight="bold"
+              sx={{
+                color: colors.greenAccent[400]
+              }} 
+            >
+              Best Bundle Pricing
+            </Typography>
+
             <Box height="40vh" width="100%">
               <LineChartGbb />
             </Box>
           </Grid>
           {/* Table */}
           <Grid item xs={12} md={6}>
-            <Header subtitle="Best Bundle Includes" />
+            <Typography
+              variant="h5" 
+              fontWeight="bold"
+              sx={{
+                color: colors.greenAccent[400]
+              }} 
+            >
+              Best Bundle Includes
+            </Typography>
+             
             <Box height="100%" width="100%">
               <TableBundle />
             </Box>

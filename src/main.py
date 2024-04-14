d1597ea4b7973aff3dbc51612a668999b5d07118
darryl-chan <darrylchan8@yahoo.com.sg>
2024-04-14 15:22:47 +0800

    Add more api functions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from controller import get_popularity, bundle_2_attraction, get_revenue_split
+from controller import get_popularity, bundle_2_attraction, get_revenue_split, get_bundle_2, get_bundle_with_highest_revenue
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -16,7 +16,8 @@ def go_popularity():
 
 @app.route("/revenue_split", methods=["GET", "POST"])
 def go_revenue_split():
-    revenue_split = get_revenue_split()
+    data = request.json ## [Attraction A, Attraction B]
+    revenue_split = get_revenue_split(data)
     return jsonify(revenue_split)
 
 @app.route("/bundle", methods=["GET", "POST"])
@@ -26,6 +27,17 @@ def go_bundle():
     bundle = bundle_2_attraction(data)
 
     return jsonify(bundle)
+
+# Find best bundles for every combination
+@app.route("/best_bundles", methods=["GET", "POST"])
+def go_find_best_bundle():
+    return jsonify(get_bundle_2())
+
+
+# Find the best bundle highest revenue
+@app.route("/highest_bundle_revenue", methods=["GET", "POST"])
+def go_find_bundle_with_highest_revenue():
+    return jsonify(get_bundle_with_highest_revenue())
 
 if __name__ == '__main__':
     app.run(debug=True)
